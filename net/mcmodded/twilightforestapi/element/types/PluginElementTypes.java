2ad62a9a72ce8b902c8db2b42deac22fe59c9b89
McModded114 <71085827+McModded114@users.noreply.github.com>
2023-06-17 13:58:51 -0400

    Create PluginElementTypes.java

---
+++ net/mcmodded/twilightforestapi/element/types/PluginElementTypes.java
@@ -0,0 +1,22 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package net.mcmodded.twilightforestapi.element.types;
+
+import net.mcreator.element.BaseType;
+import net.mcreator.element.ModElementType;
+import net.mcreator.element.ModElementTypeLoader;
+import net.nerdypuzzle.curios.ui.modgui.TwilightForestEntityGUI;
+
+public class PluginElementTypes {
+    public static ModElementType<?> TWILIGHTFORESTENTITY;
+
+    public PluginElementTypes() {
+    }
+
+    public static void load() {
+        TWILIGHTFORESTENTITY = ModElementTypeLoader.register(new ModElementType("twilightforestentity", 'B', BaseType.ENTITY, TwilightForestEntityGUI::new, TwilightForestEntity.class));
+    }
+}
